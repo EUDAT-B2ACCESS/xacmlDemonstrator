50b405d10ff091678773ca0447d1529406600d1c
a.memon <ahmed.shiraz.memon@gmail.com>
2016-09-01 15:02:35 +0200

    support for Web server deployment

---
+++ balanaDemonstrator/src/main/java/eu/eudat/b2access/authz/client/ClientHelper.java
@@ -0,0 +1,37 @@
+package eu.eudat.b2access.authz.client;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ * 
+ * Helper class to create a {@link Client} that ignores hostname checking
+ * 
+ * @author a.memon
+ *
+ */
+public class ClientHelper {
+	public static Client IgnoreSSLClient() throws Exception {
+		SSLContext sslcontext = SSLContext.getInstance("TLS");
+		sslcontext.init(null, new TrustManager[] { new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+			}
+
+			public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+			}
+
+			public X509Certificate[] getAcceptedIssuers() {
+				return new X509Certificate[0];
+			}
+
+		} }, new java.security.SecureRandom());
+		return ClientBuilder.newBuilder().sslContext(sslcontext).hostnameVerifier((s1, s2) -> true).build();
+	}
+
+}
