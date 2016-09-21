791c0ae60643446c9771e44e7b7f070c5c18ceb2
a.memon <ahmed.shiraz.memon@gmail.com>
2016-09-21 13:23:39 +0200

    use clienthelper for ssl/tls based calls

--- balanaDemonstrator/src/main/java/eu/eudat/b2access/authz/samples/DataServiceClient.java
+++ balanaDemonstrator/src/main/java/eu/eudat/b2access/authz/samples/DataServiceClient.java
@@ -5,6 +5,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 
 import eu.eudat.b2access.authz.client.ClientConfig;
+import eu.eudat.b2access.authz.client.ClientHelper;
 import eu.eudat.b2access.authz.server.XACMLServer;
 import eu.eudat.b2access.authz.server.XACMLServerConfig;
 
@@ -13,28 +14,29 @@ public class DataServiceClient {
 		XACMLServer server = new XACMLServer("src/main/conf/xacmlServer.config");
 //		localClient();
 //		remoteClient();
-		remotePdpClient();
+//		remotePdpClient();
 	}
 
 	/***
 	 * Accessing Data Service with remote PDP
+	 * @throws Exception 
 	 */
-	private static void remotePdpClient() {
-		ClientConfig config = new ClientConfig("src/test/conf/client.config");
-		Client client = ClientBuilder.newClient();
+	public void remotePdpClient(String baseAddress) throws Exception {
+//		ClientConfig config = new ClientConfig("src/test/conf/client.config");
+		Client client = ClientHelper.IgnoreSSLClient();
 		//Calling the data service with minimal rights 
-		String resultPermit = client.target("http://"+config.getHostname()+":"+config.getPort()).path("/data").queryParam("user", "alex")
+		String resultPermit = client.target(baseAddress).path("/data").queryParam("user", "alex")
 				.queryParam("group", "admin").request().get(String.class);
 		System.out.println(resultPermit);
 		
 		//Calling the data service with priviliged rights
-		String resultDeny = client.target("http://"+config.getHostname()+":"+config.getPort()).path("/data").queryParam("user", "bob")
+		String resultDeny = client.target(baseAddress).path("/data").queryParam("user", "bob")
 				.queryParam("group", "other").request().get(String.class);
 		System.out.println(resultDeny);
 		
 	}
 
-	private static void localClient() {
+	public void localClient() {
 		DataService d = new DataService();
 		// authorised operation
 		d.read("admin", "admin");
@@ -46,7 +48,7 @@ public class DataServiceClient {
 	/***
 	 * Accessing Data Service with embedded PDP
 	 */
-	private static void remoteClient() {
+	public void remoteClient() {
 		ClientConfig config = new ClientConfig("src/test/conf/client.config");
 		Client client = ClientBuilder.newClient();
 		//Calling the data service with minimal rights 
